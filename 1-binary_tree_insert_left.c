5641474375d81352cca0cb45bf7e80b0cda24fc3
Raymond <97799002+RaymondEdibo@users.noreply.github.com>
2023-10-31 21:56:27 +0100

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent)
 	{
-		nd = binary_tree_nd(parent, value);
+		nd = binary_tree_node(parent, value);
 		if (nd)
 		{
 			if (parent->left)
