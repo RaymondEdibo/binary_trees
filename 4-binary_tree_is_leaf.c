7a7072617eb9e1b63b2dcbe83544a962ecaf3f24
Raymond <97799002+RaymondEdibo@users.noreply.github.com>
2023-10-31 21:58:48 +0100

    Create 4-binary_tree_is_leaf.c

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - check node is a leaf?
+ *
+ * @node: the node
+ *
+ * Return: 1 if leaf else 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node)
+	{
+		if (!node->left && !node->right)
+			return (1);
+	}
+	return (0);
+}
