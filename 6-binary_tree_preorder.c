0d06184c6015779e8c4b83968d38d056d493bdbe
Raymond <97799002+RaymondEdibo@users.noreply.github.com>
2023-10-31 22:01:04 +0100

    Create 6-binary_tree_preorder.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - pre-order traversal through binary tree
+ *
+ * @tree: root node
+ * @func: function to call each node
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
